20eb41a17752698d2a218fc41007cad736f04edc
ParthasarathiMahana <71312465+ParthasarathiMahana@users.noreply.github.com>
2023-04-06 15:29:54 +0530

    Add files via upload

---
+++ config/environment.js
@@ -0,0 +1,11 @@
+const development = {
+    name : 'development',
+    asset_path: "./assets",
+    db: 'elderlyhealthcare'
+}
+
+const production = {
+    name : 'production'
+}
+
+module.exports = development;
